a25a51383499d234f99ec7fbb10820cd2df8d34d
Guillermo Oscar <guillermobianco9090@gmail.com>
2023-02-09 13:49:38 -0300

    Uso de nuevos métodos para trabajar con Dropdowns

---
+++ src/main/java/Dropdown/multipleSelectDropdown.java
@@ -0,0 +1,35 @@
+package Dropdown;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class multipleSelectDropdown {
+
+    public static void main(String [] args){
+
+    String url = "https://demoqa.com/select-menu";
+
+        System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\drivers\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get(url);
+        driver.manage().window().maximize();
+
+        // Declare the object of UI Select
+
+        Select cars = new Select(driver.findElement(By.id("cars")));
+
+        cars.selectByValue("audi");
+        cars.selectByVisibleText("Volvo");
+        cars.selectByIndex(2);
+
+        // Metodos para deseleccionar items
+        cars.deselectAll();
+        cars.deselectByIndex(2);
+
+        //driver.close();
+    }
+}
